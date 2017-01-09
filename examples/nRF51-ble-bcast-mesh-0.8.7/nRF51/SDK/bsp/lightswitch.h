4c27a67bd9d8d413e7f93bd821f40c4d4949f662
Temco_Heng <heng@temcocontrols.com>
2017-01-09 15:23:38 +0800

    missing ../examples/nRF51-ble-bcast-mesh-0.8.7/nRF51/SDK/bsp/lightswitch.h
    Signed-off-by: Temco_Heng <heng@temcocontrols.com>

---
+++ examples/nRF51-ble-bcast-mesh-0.8.7/nRF51/SDK/bsp/lightswitch.h
@@ -0,0 +1,79 @@
+/* Copyright (c) 2014 Nordic Semiconductor. All Rights Reserved.
+ *
+ * The information contained herein is property of Nordic Semiconductor ASA.
+ * Terms and conditions of usage are described in detail in NORDIC
+ * SEMICONDUCTOR STANDARD SOFTWARE LICENSE AGREEMENT.
+ *
+ * Licensees are granted free, non-transferable use of the information. NO
+ * WARRANTY of ANY KIND is provided. This heading must NOT be removed from
+ * the file.
+ *
+ */
+#ifndef LIGHT_SWITCH_H
+#define LIGHT_SWITCH_H
+
+// LEDs definitions for LightSwitch
+#define LEDS_NUMBER    4
+
+#define LED_START      21
+#define LED_1          21
+#define LED_2          22
+#define LED_3          23
+#define LED_4          24
+#define LED_STOP       24
+
+#define LEDS_LIST { LED_1, LED_2, LED_3, LED_4 }
+
+#define BSP_LED_0      LED_1
+#define BSP_LED_1      LED_2
+#define BSP_LED_2      LED_3
+#define BSP_LED_3      LED_4
+
+#define BSP_LED_0_MASK (1<<BSP_LED_0)
+#define BSP_LED_1_MASK (1<<BSP_LED_1)
+#define BSP_LED_2_MASK (1<<BSP_LED_2)
+#define BSP_LED_3_MASK (1<<BSP_LED_3)
+
+#define LEDS_MASK      (BSP_LED_0_MASK | BSP_LED_1_MASK | BSP_LED_2_MASK | BSP_LED_3_MASK)
+/* all LEDs are lit when GPIO is low */
+#define LEDS_INV_MASK  LEDS_MASK
+
+#define BUTTONS_NUMBER 4
+
+#define BUTTON_START   17
+#define BUTTON_1       17
+#define BUTTON_2       18
+#define BUTTON_3       19
+#define BUTTON_4       20
+#define BUTTON_STOP    20
+#define BUTTON_PULL    NRF_GPIO_PIN_PULLUP
+
+#define BUTTONS_LIST { BUTTON_1, BUTTON_2, BUTTON_3, BUTTON_4 }
+
+#define BSP_BUTTON_0   BUTTON_1
+#define BSP_BUTTON_1   BUTTON_2
+#define BSP_BUTTON_2   BUTTON_3
+#define BSP_BUTTON_3   BUTTON_4
+
+#define BSP_BUTTON_0_MASK (1<<BSP_BUTTON_0)
+#define BSP_BUTTON_1_MASK (1<<BSP_BUTTON_1)
+#define BSP_BUTTON_2_MASK (1<<BSP_BUTTON_2)
+#define BSP_BUTTON_3_MASK (1<<BSP_BUTTON_3)
+
+#define BUTTONS_MASK   0x001E0000
+
+#define RX_PIN_NUMBER  9
+#define TX_PIN_NUMBER  10
+#define CTS_PIN_NUMBER 7
+#define RTS_PIN_NUMBER 8
+#define HWFC           true
+
+#define RELAY_ON		14
+#define RELAY_OFF		13
+#define RELAY2_ON		26
+#define RELAY2_OFF		27
+
+#define IR_SEND			11		
+#define IR_REC			12
+
+#endif // PCA10028_H
